4f630091b0c750c6f4dba32e5acfbbab48c3012c
nandani17raj-a11y <nandani17raj@gmail.com>
2026-06-28 23:49:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const target=new Date('2026-07-21T00:00:00');
+const target=new Date('2026-08-21T00:00:00');
 setInterval(()=>{let d=target-new Date();if(d<0)return;
 let days=Math.floor(d/86400000),h=Math.floor(d%86400000/3600000),m=Math.floor(d%3600000/60000),s=Math.floor(d%60000/1000);
 document.getElementById('count').innerHTML=`❤️ Countdown to our meeting:<br>${days}d ${h}h ${m}m ${s}s`;},1000);
